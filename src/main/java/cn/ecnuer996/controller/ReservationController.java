87d7528a25aaaf73f2ba47f509496d73403cf9ce
Lu Wen Qing <david1113531523@gmail.com>
2019-09-21 10:34:34 +0800

    Finish first API.

--- src/main/java/cn/ecnuer996/controller/ReservationController.java
+++ src/main/java/cn/ecnuer996/controller/ReservationController.java
@@ -1,4 +1,22 @@
 package cn.ecnuer996.controller;
 
+import cn.ecnuer996.bean.Venue;
+import cn.ecnuer996.service.VenueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
 public class ReservationController {
+
+    @Autowired
+    private VenueService venueService;
+
+    @RequestMapping(value="/venue")
+    public Venue getVenue(HttpServletRequest request){
+        int venue_id=Integer.parseInt(request.getParameter("id"));
+        return venueService.getVenueById(venue_id);
+    }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! I'm David!</h2>
+</body>
+</html>
