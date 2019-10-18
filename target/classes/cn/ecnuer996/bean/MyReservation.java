680c99dec2bcf0884af9fb1e4383a48e5f6292c8
Ethereality <ethereality@vip.qq.com>
2019-10-18 12:44:10 +0800

    :fire: 重构获取订单API :fire:
    重构了获取特定用户订单的API，修改其为GET传输。
    其中日期格式问题暂未解决。
    
    Signed-off-by: Ethereality <ethereality@vip.qq.com>

---
+++ target/classes/cn/ecnuer996/bean/MyReservation.java
@@ -0,0 +1,17 @@
+package cn.ecnuer996.bean;
+
+import java.util.Date;
+
+public class MyReservation {
+    public int venue_id;
+    public String venue_name;
+    public int site_id;
+    public String site_name;
+    public Date book_time;
+    public Date date;
+    public float cost;
+    public int begin_time;
+    public int end_time;
+    public int state;
+    public String comment;
+}
---
+++ target/classes/cn/ecnuer996/service/SiteService.java
@@ -0,0 +1,18 @@
+package cn.ecnuer996.service;
+
+import cn.ecnuer996.bean.Site;
+import cn.ecnuer996.dao.SiteMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("SiteService")
+public class SiteService {
+
+    @Autowired
+    private SiteMapper siteDao;
+
+    public Site getSiteById(int siteId) {
+        return siteDao.selectByPrimaryKey(siteId);
+    }
+
+}
